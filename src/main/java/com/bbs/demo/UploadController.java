43216c80d561eca3e13272a86ec3804260944598
irang <irang0426@gmail.com>
2025-04-14 17:06:57 +0900

    UploadController mapping

--- src/main/java/com/bbs/demo/UploadController.java
+++ src/main/java/com/bbs/demo/UploadController.java
@@ -7,7 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UploadController {
 
     @GetMapping("/upload/register")
-    public String register() {
+    public String upload_register() {
         return "upload_register";
     }
+
+    @GetMapping("/upload/list")
+    public String upload_list() {
+        return "upload_list";
+    }
+
+    @GetMapping("/upload/read")
+    public String upload_read() {
+        return "upload_read";
+    }
+
 }
