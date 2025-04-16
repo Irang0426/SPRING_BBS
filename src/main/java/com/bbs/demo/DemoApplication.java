09e645fe944a95d068635e2642b9cbf80d0f1798
KU-WM <ku990732@gmail.com>
2025-04-16 15:54:31 +0900

    1. 불필요한 파일 삭제

--- src/main/java/com/bbs/demo/DemoApplication.java
+++ src/main/java/com/bbs/demo/DemoApplication.java
@@ -8,8 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-//		SpringApplication.run(HomeController.class, args);
-//		SpringApplication.run(HelloController.class, args);
 	}
 
 }
--- src/main/java/com/bbs/demo/controller/UploadController.java
+++ src/main/java/com/bbs/demo/controller/UploadController.java
@@ -1,4 +1,4 @@
-package com.bbs.demo;
+package com.bbs.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
