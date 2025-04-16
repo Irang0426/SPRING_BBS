614e45a89b66e354ce8acb7747101fdcd2d54d68
sangbin <irang0426@gmail.com>
2025-04-16 17:40:49 +0900

    boardController add RequestMapping

--- src/main/java/com/bbs/demo/controller/BoardController.java
+++ src/main/java/com/bbs/demo/controller/BoardController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
     @GetMapping("/board/register")
