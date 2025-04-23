6326f1b0ca3550abef10771fcee317c7a884b335
yuhuyn9800 <yuhyun9800@gmail.com>
2025-04-23 16:58:47 +0900

    admin 수정

--- src/main/java/com/bbs/demo/controller/AdminController.java
+++ src/main/java/com/bbs/demo/controller/AdminController.java
@@ -9,54 +9,67 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.bbs.demo.domain.Admin;
-import com.bbs.demo.mapper.AdminMapper;
+import com.bbs.demo.service.AdminService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	
 	@Autowired
-	AdminMapper adminM;
+	private AdminService adminS;
+	
+	@GetMapping("")
+	public String adminMain(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("users", adminS.getUsers(params));
+		return "admin";
+	}
+	
+	@GetMapping("/users")
+	public String users(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("users", adminS.getUsers(params));
+		return "admin_tables :: usertable";
+	}
 	
 	@GetMapping("/boards")
-	public String board(@RequestParam Map<String, String> params, Model model) {
-	    Admin pagecon = new Admin()
-	        .setOrderby(params.getOrDefault("orderby", "asc"))
-	        .setSortby(params.getOrDefault("sortby", "id"))
-	        .setPage(Integer.parseInt(params.getOrDefault("page", "0")))
-	        .setLimit(Integer.parseInt(params.getOrDefault("limit", "20")));
-	    
-		model.addAttribute("boards", adminM.findAllBoardByPage(pagecon));
-		return "admin_boards";
+	public String boards(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("boards", adminS.getBoards(params));
+		return "admin_tables :: boardtable";
+	}
+	
+	@GetMapping("/notes")
+	public String notes(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("notes", adminS.getNotes(params));
+		return "admin_tables :: notetable";
+	}
+	
+	@GetMapping("/comments")
+	public String comments(@RequestParam Map<String, String> params, Model model) {
+	    model.addAttribute("comments", adminS.getComments(params));
+		return "admin_tables :: commenttable";
+	}
+
+	@GetMapping("/users/partial")
+	public String userTablePartial(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("boards", adminS.getUsers(params));
+		return "admin_tables :: usertbody";
 	}
 	
 	@GetMapping("/boards/partial")
 	public String boardTablePartial(@RequestParam Map<String, String> params, Model model) {
-	    Admin pagecon = new Admin()
-	        .setOrderby(params.getOrDefault("orderby", "asc"))
-	        .setSortby(params.getOrDefault("sortby", "id"))
-	        .setPage(Integer.parseInt(params.getOrDefault("page", "0")))
-	        .setLimit(Integer.parseInt(params.getOrDefault("limit", "20")));
-
-	    model.addAttribute("boards", adminM.findAllBoardByPage(pagecon));
-	    return "adminboardTable :: tableBodyFragment";
+		model.addAttribute("boards", adminS.getBoards(params));
+		return "admin_tables :: boardtbody";
 	}
 	
-	@GetMapping("/notes")
-	public String notes(Model model) {
-		//model.addAttribute("notes", adminM.findAllNoteByPage());
-		return "admin_notes";
+	@GetMapping("/notes/partial")
+	public String noteTablePartial(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("boards", adminS.getNotes(params));
+		return "admin_tables :: notetbody";
 	}
 	
-	@GetMapping("/users")
-	public String users() {
-		return "admin_users";
-	}
-	
-	@GetMapping("/comments")
-	public String comments() {
-		return "admin_replies";
+	@GetMapping("/comments/partial")
+	public String commentTablePartial(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("boards", adminS.getComments(params));
+		return "admin_tables :: commenttbody";
 	}
 	
 	//삭제 후 돌아오는 요청
--- src/main/java/com/bbs/demo/mapper/AdminMapper.java
+++ src/main/java/com/bbs/demo/mapper/AdminMapper.java
@@ -6,8 +6,14 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.bbs.demo.domain.Admin;
 import com.bbs.demo.domain.Boards;
+import com.bbs.demo.domain.CommentResponse;
+import com.bbs.demo.domain.Notes;
+import com.bbs.demo.domain.Users;
 
 @Mapper
 public interface AdminMapper {
+	List<Users> findAllUserByPage(Admin admin);
 	List<Boards> findAllBoardByPage(Admin admin);
+	List<Notes> findAllNoteByPage(Admin admin);
+	List<CommentResponse> findAllCommentByPage(Admin admin);
 }
