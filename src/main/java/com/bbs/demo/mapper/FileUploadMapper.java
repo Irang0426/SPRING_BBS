c2a10e52182d7394222152d6256b466c042b0d30
irang <irang0426@gmail.com>
2025-04-28 17:34:38 +0900

    í불필요한 파일 삭제

--- src/main/java/com/bbs/demo/mapper/FileUploadMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bbs.demo.mapper;
-
-import com.bbs.demo.domain.Files;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface FileUploadMapper {
-    void insertFile(Files files);
-}
--- src/main/java/com/bbs/demo/service/FileUploadService.java
+++
@@ -1,9 +0,0 @@
-package com.bbs.demo.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface FileUploadService {
-    void storeFiles(MultipartFile[] multipartFile, int note_id) throws IOException;
-}
